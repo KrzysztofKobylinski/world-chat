6c69f65d80ab2d267df7653f6c73639ebb769ade
Krzysiek Kobyliński <kkobylinski@bevy.pl>
2019-09-14 17:01:07 +0200

    better styling of chat list

--- src/Components/PostDisplay/PostDisplay.js
+++ src/Components/PostDisplay/PostDisplay.js
@@ -2,23 +2,30 @@ import React from 'react'
 import './PostDisplay.css'
 import { ListGroupItem, Button } from 'react-bootstrap'
 import { isPoster } from '../../libs/helpers'
+import _ from 'lodash'
+import moment from 'moment'
+import ColorHash from 'color-hash'
 
 const PostDisplay = props => {
   const { translatedText,originalText} = props.post
   const bodyToShow = (translatedText) ? translatedText : originalText
+  var colorHash = new ColorHash({ lightness: 0.8 })
+  colorHash = colorHash.rgb(props.post.postUser)
+  colorHash.push(0.2)
   return (
-
-  <ListGroupItem>
-    <div className="postDisplay">
-      <div className="photoPost" style={{ backgroundImage: `url(${props.post.postAvatar})` }} />
-      <div>
-        <strong>{props.post.postUser}</strong>, napisał:
-        <strong>{isPoster(props.post.postUID, props.user.uid)}</strong>
-        <p>{bodyToShow}</p>
+    <ListGroupItem>
+      <div className="postDisplay" style={{ backgroundColor: `rgba(${_.join(colorHash, ',')})` }}>
+        <div className="photoPost" style={{ backgroundImage: `url(${props.post.postAvatar})` }} />
+        <div className="post">
+          <div className="info">
+            <div>{props.post.postUser}</div>
+            <div>{moment(props.post.postTime).format('HH:MM:SS DD/MM/YYYY')}</div>
+            {/* <div>{isPoster(props.post.postUID, props.user.uid)}</div> */}
+          </div>
+          <div className="text">{bodyToShow}</div>
+        </div>
       </div>
-    </div>
-  </ListGroupItem>
-
+    </ListGroupItem>
   )
 } 
 
