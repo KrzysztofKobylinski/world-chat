edc6bd339c9f0d52ff4458e0ede2240d4b55a49d
Oleg <orlowski.aleksander@gmail.com>
2019-09-14 17:55:09 +0200

    wip

--- src/Screens/Chat/Chat.js
+++ src/Screens/Chat/Chat.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import PostDisplay from '../../Components/PostDisplay/PostDisplay'
 import PostEditor from '../../Components/PostEditor/PostEditor'
 import _ from 'lodash'
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 import { Panel, ListGroup } from 'react-bootstrap'
 import { Button, ButtonGroup, FormGroup, InputGroup, Card } from '@blueprintjs/core'
@@ -9,6 +10,8 @@ import { Button, ButtonGroup, FormGroup, InputGroup, Card } from '@blueprintjs/c
 import './Chat.css'
 const apiKey = 'AIzaSyA1xQVYNd7WWiwAmKiBak9ZEy5RyyKJXM4'
 var googleTranslate = require('google-translate')(apiKey)
+
+
 
 class Chat extends Component {
   constructor(props) {
@@ -18,13 +21,23 @@ class Chat extends Component {
     this.state = {
       postData: []
     }
+    
   }
  
  
+  scrollToBottom = () => {
+    this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+  }
   
+  
+  
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
 
   componentDidMount() {
     this.unsubscribe = this.postsCollection.onSnapshot(this.onCollectionUpdate)
+    this.scrollToBottom();
   }
 
   componentWillUnmount() {
@@ -56,13 +69,22 @@ class Chat extends Component {
   }
   renderPostsList = (user, postData) => {
     return (
-      <ListGroup>
+      <div>
+ <ListGroup>
         {postData
           .sort((a, b) => a.postTime - b.postTime)
           .map((post, id) => {
-            return <PostDisplay key={id} user={user} post={post} />
+            return (<PostDisplay key={id} user={user} post={post} />
+              
+              )
           })}
+          
       </ListGroup>
+      <div style={{ float:"left", clear: "both" }}
+             ref={(el) => { this.messagesEnd = el; }}>
+        </div>
+      </div>
+     
     )
   }
   renderFooter = (user, posts) => <PostEditor user={user} postsCollection={posts} />
@@ -72,10 +94,16 @@ class Chat extends Component {
     const { postData } = this.state  
     return (
       <div>
+        
         <Card className="chatPanel" elevation={4}>
           {this.renderPostsList(user, postData)}
+          
+
           {this.renderFooter(user, this.postsCollection)}
+          
         </Card>
+        
+       
       </div>
     )
   }
